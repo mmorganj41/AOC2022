b8b2bf041bd7e0a5a3816b3ec8c9b14cec15d2f0
mmorganj41 <mmorganj41@gmail.com>
2023-02-19 06:14:07 -0500

    solve day 1 pt 1

--- calorie_counting_1/src/main.rs
+++ calorie_counting_1/src/main.rs
@@ -1,13 +1,34 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
-fn main() -> io::Result<()> {
-    let file = File::open("input")?;
-    let reader = BufReader::new(file);
+fn main() {
+    let reader = read_file("input");
+    let max = parse_lines(reader);
+    println!("Max Calories: {max}");
+}
+
+fn read_file(file: &str) -> BufReader<File> {
+    let file = File::open(file).expect("Could not read file");
+    BufReader::new(file)
+}
+
+fn parse_lines(reader: BufReader<File>) -> i32 {
+    let mut max = 0;
+    let mut current_total = 0;
 
     for line in reader.lines() {
-        println!("{}", line?.trim());
+        let line = line.expect("Can't read line");
+        let line = line.trim();
+        match line.parse::<i32>() {
+            Ok(n) => current_total += n,
+            Err(_) => {
+                if current_total > max {
+                    max = current_total
+                };
+                current_total = 0;
+            }
+        }
     }
 
-    Ok(())
+    max
 }
