838d160e87f3650672103e45bc94614db779d349
mmorganj41 <mmorganj41@gmail.com>
2023-02-19 12:59:32 -0500

    solve day 2 part 1

--- rock_paper_scissors_2/src/main.rs
+++ rock_paper_scissors_2/src/main.rs
@@ -12,10 +12,17 @@ static WHO_WINS: phf::Map<char, char> = phf_map! {
     'X' => 'Z',
     'Y' => 'X',
     'Z' => 'Y',
-}
+};
+
+static PLAY_POINTS: phf::Map<char, u32> = phf_map! {
+    'X' => 1,
+    'Y' => 2,
+    'Z' => 3,
+};
 
 fn main() {
-    parse_lines(read_file("input"));
+    let score = parse_lines(read_file("input"));
+    println!("Anticipated score: {score}");
 }
 
 fn read_file(file: &str) -> BufReader<File> {
@@ -23,6 +30,42 @@ fn read_file(file: &str) -> BufReader<File> {
     BufReader::new(file)
 }
 
-fn parse_lines(reader: BufReader<File>) {
-    
+fn parse_lines(reader: BufReader<File>) -> u32 {
+    let mut total_score: u32 = 0;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let line = line.trim();
+
+        let (other_move, player_move) = determine_moves(line);
+
+        total_score += PLAY_POINTS[&player_move];
+
+        total_score += get_points_from_winning(other_move, player_move);
+    }
+
+    total_score
+}
+
+fn determine_moves(line: &str) -> (char, char) {
+    let mut info = line.splitn(2, ' ');
+
+    let first_move = info.next().unwrap();
+    let mut first_move = first_move.chars().next().unwrap();
+    first_move = MOVE_DEFINITION[&first_move];
+
+    let second_move = info.next().unwrap();
+    let second_move = second_move.chars().next().unwrap();
+
+    (first_move, second_move)
+}
+
+fn get_points_from_winning(other_move: char, player_move: char) -> u32 {
+    if player_move == other_move {
+        3
+    } else if WHO_WINS[&player_move] == other_move {
+        6
+    } else {
+        0
+    }
 }
