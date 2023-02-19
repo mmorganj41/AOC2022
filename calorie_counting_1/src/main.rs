ccfcb0fbdb207ff5874d4afc51827178c654a76c
mmorganj41 <mmorganj41@gmail.com>
2023-02-19 09:53:25 -0500

    solve day 1, part 2

--- calorie_counting_1/src/main.rs
+++ calorie_counting_1/src/main.rs
@@ -1,10 +1,10 @@
 use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
+use std::io::{prelude::*, BufReader};
 
 fn main() {
     let reader = read_file("input");
     let max = parse_lines(reader);
-    println!("Max Calories: {max}");
+    println!("Max Calories: {max:?}");
 }
 
 fn read_file(file: &str) -> BufReader<File> {
@@ -13,7 +13,7 @@ fn read_file(file: &str) -> BufReader<File> {
 }
 
 fn parse_lines(reader: BufReader<File>) -> i32 {
-    let mut max = 0;
+    let mut max = [0; 3];
     let mut current_total = 0;
 
     for line in reader.lines() {
@@ -22,13 +22,31 @@ fn parse_lines(reader: BufReader<File>) -> i32 {
         match line.parse::<i32>() {
             Ok(n) => current_total += n,
             Err(_) => {
-                if current_total > max {
-                    max = current_total
-                };
+                replace_with_higher(current_total, &mut max);
                 current_total = 0;
             }
         }
     }
 
-    max
+    max.iter().sum()
+}
+
+fn replace_with_higher(current: i32, array: &mut [i32]) {
+    for i in 0..array.len() {
+        if current > array[i] {
+            if i >= array.len() - 1 {
+                insert_in_place(i, current, array);
+            }
+        } else {
+            if i > 0 {
+                insert_in_place(i - 1, current, array);
+            }
+            break;
+        }
+    }
+}
+
+fn insert_in_place(index: usize, value: i32, array: &mut [i32]) {
+    array[..=index].rotate_left(1);
+    array[index] = value;
 }
