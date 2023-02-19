abf0eb9043fb59f691fa05ca094ef4705812dae0
mmorganj41 <mmorganj41@gmail.com>
2023-02-18 23:05:33 -0500

    calorie counting day 1 project init and get input

---
+++ calorie_counting_1/target/debug/deps/calorie_counting_1-1f99e0fefb84b218.d
@@ -0,0 +1,5 @@
+/home/mmorgan/code/AOC2022/calorie_counting_1/target/debug/deps/calorie_counting_1-1f99e0fefb84b218.rmeta: src/main.rs
+
+/home/mmorgan/code/AOC2022/calorie_counting_1/target/debug/deps/calorie_counting_1-1f99e0fefb84b218.d: src/main.rs
+
+src/main.rs:
---
+++ calorie_counting_1/target/debug/deps/calorie_counting_1-39b7b6882db92296.d
@@ -0,0 +1,5 @@
+/home/mmorgan/code/AOC2022/calorie_counting_1/target/debug/deps/calorie_counting_1-39b7b6882db92296.rmeta: src/main.rs
+
+/home/mmorgan/code/AOC2022/calorie_counting_1/target/debug/deps/calorie_counting_1-39b7b6882db92296.d: src/main.rs
+
+src/main.rs:
