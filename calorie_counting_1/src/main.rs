4931d0a5a23bc431c75b8c87f7b2031ef6852870
mmorganj41 <mmorganj41@gmail.com>
2023-02-18 23:15:46 -0500

    read input file

--- calorie_counting_1/src/main.rs
+++ calorie_counting_1/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn main() -> io::Result<()> {
+    let file = File::open("input")?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("{}", line?.trim());
+    }
+
+    Ok(())
 }
---
+++ calorie_counting_1/target/debug/calorie_counting_1.d
@@ -0,0 +1,1 @@
+/home/mmorgan/code/AOC2022/calorie_counting_1/target/debug/calorie_counting_1: /home/mmorgan/code/AOC2022/calorie_counting_1/src/main.rs
