9b6205f2a9e2dc26ae4b4086c36502a4c3df8ac2
mmorganj41 <mmorganj41@gmail.com>
2023-02-18 23:20:33 -0500

    remove target from git

--- calorie_counting_1/target/debug/calorie_counting_1.d
+++
@@ -1,1 +0,0 @@
-/home/mmorgan/code/AOC2022/calorie_counting_1/target/debug/calorie_counting_1: /home/mmorgan/code/AOC2022/calorie_counting_1/src/main.rs
--- calorie_counting_1/target/debug/deps/calorie_counting_1-134b22618056e181.d
+++
@@ -1,5 +0,0 @@
-/home/mmorgan/code/AOC2022/calorie_counting_1/target/debug/deps/calorie_counting_1-134b22618056e181: src/main.rs
-
-/home/mmorgan/code/AOC2022/calorie_counting_1/target/debug/deps/calorie_counting_1-134b22618056e181.d: src/main.rs
-
-src/main.rs:
